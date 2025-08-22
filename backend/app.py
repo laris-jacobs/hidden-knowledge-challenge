f69d5f1adfcb8fc6bb00e9c084e4127665d702ba
ben chefson <chef@chef.chef>
2025-08-22 21:50:46 +0200

    mini backend

---
+++ backend/app.py
@@ -0,0 +1,43 @@
+import datetime
+import subprocess
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+def log(msg):
+    print(f"[server] [{datetime.datetime.now().replace(microsecond=0).isoformat()}] {msg}")
+
+
+@app.route('/')
+def index():
+    log("index...")
+    data = {"somearray": ["aaa", "bbb"]}
+    return jsonify(data)
+
+
+
+def runshell(cmd):
+    output = ""
+    log(f"run: {cmd}")
+    cupsresult = subprocess.run(
+        cmd,
+        shell=True,
+        text=True,
+        capture_output=True,
+    )
+    output += f"<br>stdout: {cupsresult.stdout}\n"
+    output += f"<br>stderr: {cupsresult.stderr}\n"
+    return output
+
+
+
+def serverstart():
+    log("serverstart...")
+    log("testrunshell: " + runshell("ls -la"))
+
+
+serverstart()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
