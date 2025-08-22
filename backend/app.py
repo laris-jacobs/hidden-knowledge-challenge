7d125f74dc07b17af5d616552d89f168097bd9cc
ben chefson <chef@chef.chef>
2025-08-22 23:52:27 +0200

    .

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 import datetime
 import subprocess
-
+from dotenv import load_dotenv
 from flask import Flask, jsonify
-
+import pyodbc
 app = Flask(__name__)
 
 
@@ -17,6 +17,25 @@ def index():
     return jsonify(data)
 
 
+def get_connection():
+    log("drivers: " )
+    for d in pyodbc.drivers():
+        log("d: " +d )
+    log("drivers-end " )
+    odbc_string = """Driver={ODBC Driver 18 for SQL Server};Server=tcp:bernhaeckt-sql.database.windows.net,1433;Database=Bernhaeckt;Uid=bernhaeckt;Pwd=Unisysch2025;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
+
+    try:
+        connection = pyodbc.connect(odbc_string)
+        return connection
+    except pyodbc.Error as e:
+        app.logger.error(f"Database connection error: {e}")
+        raise
+
+def sql_one(query):
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute(query)
+        return cursor.fetchone()
 
 def runshell(cmd):
     output = ""
@@ -36,6 +55,8 @@ def runshell(cmd):
 def serverstart():
     log("serverstart...")
     log("testrunshell: " + runshell("ls -la"))
+    log("select 1 result: " + str( sql_one("select 1")))
+    log("select schemas result: " + str(sql_one("select schema_name from information_schema.schemata")))
 
 
 serverstart()
