96183fb3d3d7e4ca339fdc2fa828e7f0a7ad40fa
Moria Tatdja <morarmel@yahoo.fr>
2025-08-23 21:16:33 +0200

    footer and css

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,5 @@
 import {Component, signal} from '@angular/core';
-import {EChartsOption} from 'echarts';
+
 
 @Component({
   selector: 'app-root',
@@ -9,28 +9,4 @@ import {EChartsOption} from 'echarts';
 })
 export class App {
   protected readonly title = signal('hiddenKnowledge');
-  chartOption: EChartsOption = {
-    title: { text: 'Knowledge Graph Example' },
-    tooltip: {},
-    series: [
-      {
-        type: 'graph',
-        layout: 'force',
-        roam: true, // zoom & pan
-        label: { show: true },
-        force: { repulsion: 200 },
-        data: [
-          { name: 'Angular', symbolSize: 50 },
-          { name: 'TypeScript', symbolSize: 40 },
-          { name: 'RxJS', symbolSize: 30 },
-          { name: 'NgRx', symbolSize: 30 }
-        ],
-        links: [
-          { source: 'Angular', target: 'TypeScript' },
-          { source: 'Angular', target: 'RxJS' },
-          { source: 'Angular', target: 'NgRx' }
-        ]
-      }
-    ]
-  };
 }
